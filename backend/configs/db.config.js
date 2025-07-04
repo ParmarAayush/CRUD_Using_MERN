552a76744f246c1433f0848800b77372343f61fe
ParmarAayush <parmarayush1816@gmail.com>
2025-07-04 22:44:34 +0530

    updated databse url

--- backend/configs/db.config.js
+++ backend/configs/db.config.js
@@ -1,6 +1,6 @@
 const dbConfig = {
   DB_NAME: "wanderlust",
-  DB_URL: "mongodb+srv://parmarayush1816:NN2zpS5Ig1NX2arE@cluster0.zmpzv7x.mongodb.net/?retryWrites=true&w=majority&ssl=true&appName=Cluster0",
+  DB_URL: "mongodb+srv://parmarayush1816:crudtest@cluster0.xmbmwo2.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0",
 };
 
 export { dbConfig };
